a62d442058ad1fdf8de37e830c7c8bcb4a2c239d
dpsvadodara <techtrekdpsv@gmail.com>
2025-11-06 18:03:01 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -142,6 +142,6 @@ app.get(
 
 
 // Start the server
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server running on port ${process.env.PORT || 3000}`);
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
